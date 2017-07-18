7f58084d03a412a2ed9f5807504574ae685386be
CorvoElefante <corvoelefante@gmail.com>
2017-07-18 12:12:12 -0400

    Adiocionados os arquivos de conexão com o banco de dados

---
+++ Estagio/ControLab/ControLab/ISQLite.cs
@@ -0,0 +1,13 @@
+﻿using SQLite.Net;
+
+namespace ControLab
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+        SQLiteConnection GetConnection(string DBFileName);
+        string GetDBPath();
+        void SaveDBFile(string fileName, byte[] file);
+        bool FileExists(string fileName);
+    }
+}
