366d9909bfc489fbf449750be7e38bb6bbadfee1
TAS <TAS@DESKTOP-CM1RK07>
2017-07-18 12:30:12 -0400

    tabelas criadas: Usuarios Sala LogSala DataSala ArCondicionado Interruptores

---
+++ Estagio/ControLab/ControLab/Models/Interruptores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
+
+namespace ControLab.Models
+{
+    public partial class Usuarios
+    {
+        [PrimaryKey, AutoIncrement]
+        public long? Id { get; set; }
+
+        public long? IdSala { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ Estagio/ControLab/ControLab/Models/LogLab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
+
+namespace ControLab.Models
+{
+    public partial class Usuarios
+    {
+        [PrimaryKey, AutoIncrement]
+        public long? Id { get; set; }
+
+        public long? IdUsuario { get; set; }
+
+        public long? IdSala { get; set; }
+
+        public int Evento { get; set; }
+
+        public DateTime Instante { get; set; }
+    }
+}
