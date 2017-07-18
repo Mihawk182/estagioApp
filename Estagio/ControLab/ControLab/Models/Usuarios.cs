e16221ca566de5339c24ab92bf6e5b7ec16b7150
CorvoElefante <corvoelefante@gmail.com>
2017-07-18 12:02:52 -0400

    Criada a model da tabela de Usuarios

---
+++ Estagio/ControLab/ControLab/Models/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
+
+namespace ControLab.Models
+{
+    public partial class Usuarios
+    {
+        [PrimaryKey, AutoIncrement]
+        public long? Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public int Privilegios { get; set; } 
+    }
+}
