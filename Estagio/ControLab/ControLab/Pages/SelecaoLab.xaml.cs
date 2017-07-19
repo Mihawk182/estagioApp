82f0c6dd7c758c7e4cbb85ddad5c9d34c23ea771
TAS <TAS@DESKTOP-CM1RK07>
2017-07-19 14:23:57 -0400

    criando pagina SelecaoLab SelecaoLabViewModel

---
+++ Estagio/ControLab/ControLab/Pages/SelecaoLab.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ControLab.ViewMoldes;
+
+namespace ControLab.Pages
+{
+    public partial class SelecaoLab : ContentPage
+    {
+        public SelecaoLab()
+        {
+            BindingContext = new SelecaoLabViewModel(Navigation);
+            InitializeComponent();
+        }
+    }
+}
---
+++ Estagio/ControLab/ControLab/ViewMoldes/SelecaoLabViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using ControLab.Models;
+using System.Collections.Generic;
+using ControLab.Repositories;
+using System.Threading.Tasks;
+using ControLab.Pages;
+
+namespace ControLab.ViewMoldes
+{
+    public class SelecaoLabViewModel : BaseViewModel
+    {
+        public SelecaoLabViewModel(INavigation navigation) : base(navigation)
+        {
+        }
+
+        Command _AvancarLabCommand;
+        public Command AvancarLabCommand
+        {
+            get { return _AvancarLabCommand ?? (_AvancarLabCommand = new Command(async () => await ExecuteAvancarLabCommand())); }
+        }
+
+        async Task ExecuteAvancarLabCommand()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+                await Navigation.PushAsync(new SelecaoLab());
+                IsBusy = false;
+            }
+        }
+    }
+
+}
