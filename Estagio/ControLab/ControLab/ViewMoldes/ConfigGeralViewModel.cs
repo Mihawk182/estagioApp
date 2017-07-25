c4dcbdfb894381105c9c33c0be39b212fb82e463
TAS <TAS@DESKTOP-CM1RK07>
2017-07-25 17:59:08 -0400

    Botao acessa link e liga o led

--- Estagio/ControLab/ControLab/ViewMoldes/ConfigGeralViewModel.cs
+++ Estagio/ControLab/ControLab/ViewMoldes/ConfigGeralViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using ControLab.Repositories;
 using System.Threading.Tasks;
 using ControLab.Pages;
+using System.Net;
 
 namespace ControLab.ViewMoldes
 {
@@ -13,6 +14,32 @@ namespace ControLab.ViewMoldes
     {
         public ConfigGeralViewModel(INavigation navigation) : base(navigation)
         {
+        }
+
+        Command _LigarLampadaI;
+        public Command LigarLampadaI
+        {
+            get { return _LigarLampadaI ?? (_LigarLampadaI = new Command(async () => await ExecuteLigarLampadaI())); }
+        }
+
+        async Task ExecuteLigarLampadaI()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+
+                var request =  HttpWebRequest.Create(string.Format(@"http://192.168.1.182/?function=led4_on"));
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                }
+                
+                IsBusy = false;
+            }
         }
 
         Command _AvancarConfigArCommand;
